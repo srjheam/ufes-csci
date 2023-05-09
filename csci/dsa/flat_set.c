8aac5fb04f9887f3b325bcf47b3dbf4d17233bb0
Jheam Storch Ross <jheam.storch@hotmail.com>
2023-05-09 11:31:03 -0300

    Update flat_set

--- csci/dsa/flat_set.c
+++ csci/dsa/flat_set.c
@@ -3,10 +3,15 @@
 #define FLAT_SET_INITIAL_SIZE 16
 #define FLAT_SET_GROWTH_FACTOR 2
 
-flat_set *fset_constructor(size_t smemb) {
+flat_set *fset_constructor(size_t smemb, compar_fn compar, copy_fn copy,
+                           destruct_fn destruct) {
     flat_set *fset = malloc(sizeof *fset);
 
     fset->smemb = smemb;
+
+    fset->compar = compar;
+    fset->copy = copy;
+    fset->destruct = destruct;
 
     fset->size = FLAT_SET_INITIAL_SIZE;
     fset->len = 0;
@@ -17,7 +22,32 @@ flat_set *fset_constructor(size_t smemb) {
 
 size_t fset_length(flat_set *fset) { return fset->len; }
 
+void *fset_index(flat_set *fset, size_t idx) {
+    return fset->arr[idx * fset->smemb];
+}
+
+size_t fset_iterator_begin(flat_set *fset) { return 0; }
+
+void *fset_iterator_next(flat_set *fset, size_t *saveptr) {
+    void *curr = fset_index(fset, *saveptr);
+
+    *saveptr++;
+
+    return curr;
+}
+
+bool fset_iterator_has_next(flat_set *fset, size_t *saveptr) {
+    return fset_length(fset) > *saveptr;
+}
+
 void fset_destructor(flat_set *fset) {
+    size_t it = fset_iterator_begin(fset);
+    while (fset_iterator_has_next(fset, &it)) {
+        void *curr = fset_iterator_next(fset, &it);
+
+        fset->destruct(curr);
+    }
+
     free(fset->arr);
 
     free(fset);
--- csci/dsa/flat_set.h
+++ csci/dsa/flat_set.h
@@ -9,12 +9,27 @@ typedef struct {
     size_t len;
     size_t size;
 
+    compar_fn compar;
+    copy_fn copy;
+    destruct_fn destruct;
+
     size_t smemb;
 } flat_set;
 
-flat_set *fset_constructor(size_t smemb);
+flat_set *fset_constructor(size_t smemb, compar_fn compar, copy_fn copy,
+                           destruct_fn destruct);
 
 size_t fset_length(flat_set *fset);
+
+void *fset_index(flat_set *fset, size_t idx);
+
+void fset_push(flat_set *fset, void *elem);
+
+size_t fset_iterator_begin(flat_set *fset);
+
+void *fset_iterator_next(flat_set *fset, size_t *saveptr);
+
+bool fset_iterator_has_next(flat_set *fset, size_t *saveptr);
 
 void fset_destructor(flat_set *fset);
 
