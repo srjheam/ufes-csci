1592ddc5e9464cfdb752b0fa51852762fb93c5b4
Jheam Storch Ross <jheam.storch@hotmail.com>
2023-05-09 11:30:21 -0300

    Update common

--- csci/dsa/common.h
+++ csci/dsa/common.h
@@ -3,7 +3,14 @@
 
 #include "stdlib.h"
 #include "string.h"
+#include "stdbool.h"
 
 typedef unsigned char byte;
+
+typedef int (*compar_fn)(const void *, const void *);
+
+typedef void *(*copy_fn)(const void *);
+
+typedef void (*destruct_fn)(void *);
 
 #endif
