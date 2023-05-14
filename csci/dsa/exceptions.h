5557024583f3fa4ede3d7d3e83a3f6b3abebc357
Jheam Storch Ross <jheam.storch@hotmail.com>
2023-05-14 18:10:31 -0300

    Add exceptions

---
+++ csci/dsa/exceptions.h
@@ -0,0 +1,15 @@
+#ifndef _EXCEPTIONS_H_
+#define _EXCEPTIONS_H_
+
+#define EXIT_OOM 137
+
+void exception_throw(char *name, char *msg, int code);
+
+void exception_throw_index(char *msg);
+
+void exception_throw_oom(char *msg);
+
+void exception_throw_failure(char *msg);
+
+
+#endif
