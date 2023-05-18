63d894d7f3ae121fe48f1b4478525bdcb460dff5
Jheam Storch Ross <jheam.storch@hotmail.com>
2023-05-18 18:41:05 -0300

    Get test

--- csci/tests/main.c
+++ csci/tests/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include "csci/csrc_matrix.h"
 #include "csci/csrc_matrix_iterator.h"
@@ -37,6 +38,16 @@ int main() {
     csrc_matrix_print_sparse(mat);
 
     csrc_matrix_print_dense(mat);
+
+    assert(csrc_matrix_get(mat, 0, 0) == 1);
+    assert(csrc_matrix_get(mat, 0, 1) == 0);
+    assert(csrc_matrix_get(mat, 0, 2) == 3);
+    assert(csrc_matrix_get(mat, 1, 0) == 0);
+    assert(csrc_matrix_get(mat, 1, 1) == 5);
+    assert(csrc_matrix_get(mat, 1, 2) == 6);
+    assert(csrc_matrix_get(mat, 2, 0) == 7);
+    assert(csrc_matrix_get(mat, 2, 1) == 8);
+    assert(csrc_matrix_get(mat, 2, 2) == 0);
 
     csrc_matrix_destructor(mat);   
 
