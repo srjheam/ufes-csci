bf298796a1bc3b3ca9c85fe35a931adb8a0c5d38
Jheam Storch Ross <jheam.storch@hotmail.com>
2023-05-21 01:58:09 -0300

    Add test-all

---
+++ csci/tests/test-all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 [-m]"
+   echo -e "\t-m Run with valgrind, 1 for true, 0 for false"
+   exit 1
+}
+
+VALGRIND=0
+
+while getopts "m:" opt
+do
+   case "$opt" in
+      m ) VALGRIND="$OPTARG" ;;
+      ? ) helpFunction ;;
+   esac
+done
+
+for d in */ ; do
+    d="${d%/}"
+    sh run-test.sh -e $d -m $VALGRIND
+done
