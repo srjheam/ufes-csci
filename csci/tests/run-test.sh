d91558de97220e852b40510abea567e439469044
Jheam Storch Ross <jheam.storch@hotmail.com>
2023-05-21 01:22:43 -0300

    Val on run-tests

--- csci/tests/run-test.sh
+++ csci/tests/run-test.sh
@@ -9,15 +9,19 @@ LIGHT_GREEN='\033[1;32m'
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -e <test>"
+   echo "Usage: $0 -e <test> [-m]"
    echo -e "\t-e Test directory name (e.g. dot_test)"
+   echo -e "\t-m Run with valgrind, 1 for true, 0 for false"
    exit 1
 }
 
-while getopts "e:" opt
+VALGRIND=0
+
+while getopts "e:m:" opt
 do
    case "$opt" in
       e ) TEST="$OPTARG" ;;
+      m ) VALGRIND="$OPTARG" ;;
       ? ) helpFunction ;;
    esac
 done
@@ -46,8 +50,20 @@ do
     echo -n "#$NTEST"
     OUT=`$TARGET < $INPUT`
     DIFF=`echo "$OUT" | diff $EXPECTED -`
+    if [ $VALGRIND -eq 1 ]
+    then        
+        (valgrind --leak-check=full \
+			 --show-leak-kinds=all \
+			 --track-origins=yes \
+			 --verbose \
+			 --log-file=$TEST/valgrind-out.txt \
+			 --error-exitcode=1 \
+			 $TARGET < $INPUT 1> /dev/null)
 
-    if [ -z "${DIFF}" ]
+         VAL_OUT=$?
+    fi
+
+    if [ -z "${DIFF}" ] && [ $VAL_OUT -eq 0 ]
     then
         echo " ${LIGHT_GREEN}OK${NC}"
     else
@@ -61,13 +77,30 @@ do
         echo "$OUT"
         echo ""
 
-        echo "${YELLOW}Expected${NC}"
-        cat $EXPECTED
-        echo ""
+        if [ -n "${DIFF}" ]
+        then
+            echo "${YELLOW}Expected${NC}"
+            cat $EXPECTED
+            echo ""
 
-        echo "${YELLOW}Diff${NC}"
-        echo $DIFF
-        echo ""
+            echo "${YELLOW}Diff${NC}"
+            echo $DIFF
+            echo ""
+        else
+            echo "${YELLOW}Expected${NC}"
+            echo ""
+
+            echo "${YELLOW}Diff${NC}"
+            echo ""
+        fi
+
+        if [ $VALGRIND -eq 1 ] && [ $VAL_OUT -ne 0 ]
+        then
+            echo "${YELLOW}Valgrind${NC}"
+            echo "Leaks detected!"
+            echo "Head to ${TEST_DIR}valgrind-out.txt"
+            echo ""
+        fi
         break
     fi
 done
